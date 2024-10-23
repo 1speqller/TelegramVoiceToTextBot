2fb60748d2785d4cd30cca8fee2861a79720a565
1speqller <hoshigake2000@gmail.com>
2024-10-23 11:13:22 +0300

    Добавьте файлы проекта.

---
+++ VoiceTexterBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+namespace VoiceTexterBot
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() 
+                .Build();
+
+            Console.WriteLine("Сервис запущен");
+            await host.RunAsync();
+
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("8055439169:AAH41-QxN4_GFd8YA9P9tE0Yy6YebF4osRc"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+    }
+}
